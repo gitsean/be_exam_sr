7b86ca2cd5ca1a6bb410fb45ece2cec47ea2078e
gitSean <sareid@outlook.com>
2018-11-17 05:54:44 -0500

    Babel for jest testing

--- services/detect.input.test.js
+++ services/detect.input.test.js
@@ -1,3 +1,16 @@
-test("tests should run", () => {
+import fs from "fs";
+import path from "path";
+import { promisify } from "util";
+process.env.DETECTED = false;
+
+const write = promisify(fs.appendFile);
+
+test("test should create a file in input dir", async () => {
+  const p = path.join(__dirname, "..", "input", "/test.txt");
+  try {
+    await write(p, "Not much here. Terribly sorry.", "utf-8");
+  } catch (e) {
+    console.log("error: ", e);
+  }
   expect(true).toBe(true);
 });
